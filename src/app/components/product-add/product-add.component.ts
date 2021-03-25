460ef81db0c84dadd7ce2dd134bd31584d532726
Nazif Kanık <nazifkanik@gmail.com>
2021-03-25 19:08:44 +0300

    25.03.2021

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -31,14 +31,16 @@ export class ProductAddComponent implements OnInit {
   add() {
     if (this.productAddForm.valid) {
       let productModel = Object.assign({}, this.productAddForm.value)
-      this.productService.add(productModel).subscribe(response=>{
-        console.log(response)
+      this.productService.add(productModel).subscribe(response => {
         this.toastrService.success(response.message, "Başarılı")
-      },responseError=>{
-        console.log(responseError.error)
-        this.toastrService.error(responseError.error)
+      }, responseError => {
+        if (responseError.error.ValidationErrors.length > 0) {
+          for (let i = 0; i < responseError.error.ValidationErrors.length; i++) {
+            this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage, "Doğrulama hatası")
+          }
+        }
       })
-      
+
     }
     else {
       this.toastrService.error("Formunuz eksik", "Dikkat")
